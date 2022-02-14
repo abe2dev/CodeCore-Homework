4e89e13bad7c7054eaad9ba71156f9dcd0412381
Ibrahim <98372964+abe2dev@users.noreply.github.com>
2022-02-13 17:48:07 -0800

    Create save action

--- to_do_cli/todoCLI.js
+++ to_do_cli/todoCLI.js
@@ -1,94 +1,104 @@
-const readline = require("readline")
+const fs = require("fs");
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "> " // the shape of the prompt > also we can put %
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ", // the shape of the prompt > also we can put %
 });
- 
+
 const theList = [];
 
-
-function menuPrompt(){
-    const theMenu = ( "(v) View.(n) New.(cX) Complete.(dX) Delete,(q) Quit")
-    console.log(theMenu);
-    rl.prompt()
+function menuPrompt() {
+  const theMenu =
+    "(v) View. (n) New. (cX) Complete. (dX) Delete. (s) Save . (q) Quit";
+  console.log(theMenu);
+  rl.prompt();
 }
 
-
-function showList(){
-        if (theList.length <= 0) {
-            console.log("List is empty...");
-
-        } else {
-        theList.forEach((entry, index) => {
-        let status = entry[0] === true ? "✔": " ";
-        console.log(`${index} [${status}] ${entry[1]}`)
+function showList() {
+  if (theList.length <= 0) {
+    console.log("List is empty...");
+  } else {
+    theList.forEach((entry, index) => {
+      let status = entry.complete === true ? "✔" : " ";
+      console.log(`${index} [${status}] ${entry.title}`);
     });
   }
-        
 }
 
-
 function newEntry(entry = "") {
-    if (entry === "") {
-        console.log("What?");
-        rl.prompt()
-    } else {
-        theList.push([false, entry])
-    }
-
+  if (entry === "") {
+    console.log("What?");
+    rl.prompt();
+  } else {
+    theList.push({ complete: false, title: entry });
+  }
 }
 
 function completeItem(num) {
-    if (num >= 0 && num < theList.length) {
-       theList[num][0] = true;
-       console.log(`Completed "${theList[num][1]}"`); 
-    }
-   
+  if (num >= 0 && num < theList.length) {
+    theList[num].complete = true;
+    console.log(`Completed "${theList[num].title}"`);
+  }
 }
 
 function deleteItem(num) {
-    if (num >= 0 && num < theList.length) {
-       console.log(`Deleted "${theList.splice(num, 1)[0][1]}"`); 
-    }
+  if (num >= 0 && num < theList.length) {
+    const deletedItem = theList.splice(num, 1)[0];
+    console.log(`Deleted "${deletedItem.title}"`);
+  }
 }
 
-
-
+function saveList(filename = "") {
+  if (filename === "") {
+    console.log("Where? (myTodos.json)");
+    rl.prompt();
+  } else {
+    fs.writeFileSync(`./${filename}`, JSON.stringify(theList));
+      console.log(`List saved to "${filename}"`);
+  }
+}
 
 console.log("Welcome to Todo CLI!\n--------------------");
 menuPrompt();
 
 let curCmd = "";
 rl.on("line", (input) => {
-
-  if (input === "q") {
-      console.log("See you soon! 😊")
+  if (input === "q" && curCmd === "") {
+    console.log("See you soon! 😊");
     rl.close();
     return;
-
-  } else if (input === "v") {
-    showList()
+  } else if (input === "v" && curCmd === "") {
+    showList();
     menuPrompt();
-
-  } else if (input === "n"){
+  } else if (input === "n" && curCmd === "") {
     curCmd = "n";
     newEntry();
-   
-  } else if ( curCmd === "n"){
-      newEntry(input)
-      curCmd = "";
-      menuPrompt();
-
+  } else if (curCmd === "n") {
+    newEntry(input);
+    curCmd = "";
+    menuPrompt();
   } else if (input[0] === "c") {
-      let num = parseInt(input.slice(1))
-      completeItem(num);
-      menuPrompt();
+    let num = parseInt(input.slice(1));
+    completeItem(num);
+    menuPrompt();
   } else if (input[0] === "d") {
-    let num = parseInt(input.slice(1))
+    let num = parseInt(input.slice(1));
     deleteItem(num);
     menuPrompt();
-}
-
+  } else if (input === "s" && curCmd === "") {
+    curCmd = "s";
+    saveList();
+  } else if (curCmd === "s") {
+    if (input === "") {
+      input = "myTodos.JSON"
+      
+    }
+    saveList(input);
+    curCmd = "";
+    menuPrompt();
+  } else if (curCmd === "") {
+    menuPrompt();
+  }
 });

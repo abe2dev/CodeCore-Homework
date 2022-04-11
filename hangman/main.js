ae138189f691e4604d724ae13c39e6ef7af2c435
ibrahim <ibrahimood1@gmail.com>
2022-04-10 18:24:12 -0700

    Correct Guesses

--- hangman/main.js
+++ hangman/main.js
@@ -1,10 +1,35 @@
 $(document).ready(() => {
-  function alphabetClick(event) {
-    event.target.style.backgroundColor = "orangered";
-    event.target.style.color = "white";
-    event.target.style.border = "none";
-  }
-  document.querySelectorAll("td").forEach(element => {
+  const mysteryWord = "STRANGER"
+  const letterTag = "td"
+
+  document.querySelectorAll("#keyboard td").forEach(element => {
     element.addEventListener("click", alphabetClick)
   });
+
+  const mysteryWordElement = document.querySelector("#mysteryWord tr")
+  for (let i = 0; i < mysteryWord.length; i++) {
+    mysteryWordElement.appendChild(document.createElement(letterTag))
+  }
+
+  function showTheLetters(letter) {
+    let index = mysteryWord.indexOf(letter)
+    while (index > -1) {
+      mysteryWordElement.children[index].innerText = letter
+      index = mysteryWord.indexOf(letter, index+1)
+    }
+  }
+
+  function showTheHangman() { }
+
+  function alphabetClick(event) {
+    const letterElement = event.target
+    if (letterElement.classList.contains("selected-letter")) {
+
+    }
+    else {
+      letterElement.classList.add("selected-letter");
+      const letter = letterElement.innerText
+      mysteryWord.includes(letter) ? showTheLetters(letter) : showTheHangman();
+    }
+  }
 });

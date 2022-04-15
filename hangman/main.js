4f992d2e040b330457d3b0d2ab6c378464b36884
ibrahim <ibrahimood1@gmail.com>
2022-04-14 19:58:50 -0700

    Final Code + Adding sounds

--- hangman/main.js
+++ hangman/main.js
@@ -1,35 +1,106 @@
-$(document).ready(() => {
-  const mysteryWord = "STRANGER"
-  const letterTag = "td"
+var newZealandCities = [
+"auckland",
+"wellington",
+"christchurch",
+"hamilton",
+"napierhastings",
+]
 
-  document.querySelectorAll("#keyboard td").forEach(element => {
-    element.addEventListener("click", alphabetClick)
-  });
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let audio;
+let wordStatus = null;
 
-  const mysteryWordElement = document.querySelector("#mysteryWord tr")
-  for (let i = 0; i < mysteryWord.length; i++) {
-    mysteryWordElement.appendChild(document.createElement(letterTag))
-  }
+function randomWord() {
+    answer = newZealandCities[Math.floor(Math.random() * newZealandCities.length)];
+}
 
-  function showTheLetters(letter) {
-    let index = mysteryWord.indexOf(letter)
-    while (index > -1) {
-      mysteryWordElement.children[index].innerText = letter
-      index = mysteryWord.indexOf(letter, index+1)
+
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+        <button
+          class="btn btn-outline-warning active m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')" >
+          ` + letter + `
+        </button>
+      `).join('');
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updateHangmanPicture();
     }
-  }
+}
 
-  function showTheHangman() { }
 
-  function alphabetClick(event) {
-    const letterElement = event.target
-    if (letterElement.classList.contains("selected-letter")) {
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+}
+
+
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById('sound-win').play();
+        document.getElementById('keyboard').innerHTML = "";
+        document.getElementById('keyboard').innerHTML = alert("Congratulations!You won!");
+        document.getElementById('keyboard').innerHTML = "Let's Play again! ";
+
 
     }
-    else {
-      letterElement.classList.add("selected-letter");
-      const letter = letterElement.innerText
-      mysteryWord.includes(letter) ? showTheLetters(letter) : showTheHangman();
+}
+
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('sound-lose').play();
+        document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = alert("Better luck next time... ");
+        document.getElementById('keyboard').innerHTML = "Let's Play again! ";
     }
-  }
-});
+}
+
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.jpg';
+
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+}
+
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
